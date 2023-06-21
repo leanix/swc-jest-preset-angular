dfc476a56cf262b7fb08a0d9cc2051f31b4feb98
Rene Hamburger <rene.hamburger@leanix.net>
2023-06-21 15:11:21 +0200

    Remove style attribute completely, as some tests now require this

--- src/transformers/swc/swc-preprocessor.spec.ts
+++ src/transformers/swc/swc-preprocessor.spec.ts
@@ -7,73 +7,81 @@ const sources = [
   styleUrls: [ '../media-box.component.scss' ],
 })`,
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: './media-box-h0.component.html',
-  styleUrls: ['../media-box.component.scss',
-  './media-box-h0.component.scss'],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: './media-box-h0.component.html',
+    styleUrls: ['../media-box.component.scss',
+    './media-box-h0.component.scss'],
+  })`,
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: 'media-box-h0.component.html',
-  styleUrls: [
-    '../media-box.component.scss',
-  ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: 'media-box-h0.component.html',
+    styleUrls: [
+      '../media-box.component.scss',
+    ],
+  })`,
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: 'media-box-h0.component.html',
-  styleUrls: [
-    '../media-box.component.scss',
-    './media-box-h0.component.scss'
-  ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: 'media-box-h0.component.html',
+    styleUrls: [
+      '../media-box.component.scss',
+      './media-box-h0.component.scss'
+    ],
+  })`,
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: 'media-box-h0.component.html',
-  styleUrls: [
-    '../../box.component.scss',
-    '../media-box.component.scss',
-    './media-box-h0.component.scss'
-  ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: 'media-box-h0.component.html',
+    styleUrls: [
+      '../../box.component.scss',
+      '../media-box.component.scss',
+      './media-box-h0.component.scss'
+    ],
+  })`,
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: 'media-box-h0.component.html',
-  styleUrls: [
-    '../../../box.component.scss',
-    '../../box.component.scss',
-    '../media-box.component.scss',
-    './media-box-h0.component.scss'
-  ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: 'media-box-h0.component.html',
+    styleUrls: [
+      '../../../box.component.scss',
+      '../../box.component.scss',
+      '../media-box.component.scss',
+      './media-box-h0.component.scss'
+    ],
+  })`,
   `@Component({
-  selector    : 'xc-media-box-h0',
-  templateUrl : 'media-box-h0.component.html',
-  styleUrls   : [
-    '../../../box.component.scss',
-    '../../box.component.scss',
-    '../media-box.component.scss',
-    './media-box-h0.component.scss'
-  ],
-})`,
+    selector    : 'xc-media-box-h0',
+    templateUrl : 'media-box-h0.component.html',
+    styleUrls   : [
+      '../../../box.component.scss',
+      '../../box.component.scss',
+      '../media-box.component.scss',
+      './media-box-h0.component.scss'
+    ],
+  })`,
   // double quote
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: "./media-box-h0.component.html",
-  styleUrls: [ '../media-box.component.scss' ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: "./media-box-h0.component.html",
+    styleUrls: [ '../media-box.component.scss' ],
+  })`,
   // backtick
   `@Component({
-  selector: 'xc-media-box-h0',
-  templateUrl: \`./media-box-h0.component.html\`,
-  styleUrls: [ '../media-box.component.scss' ],
-})`,
+    selector: 'xc-media-box-h0',
+    templateUrl: \`./media-box-h0.component.html\`,
+    styleUrls: [ '../media-box.component.scss' ],
+  })`,
+  // styles
+  `@Component({
+    selector: 'xc-media-box-h0',
+    templateUrl: \`./media-box-h0.component.html\`,
+    styles: []
+  })`,
 ];
 
 describe('preprocessFileContent', () => {
   test.each(sources)('removes styleUrls and imports template for %s', (source) => {
-    const result = preprocessFileContent(source, '');
-    expect(result).toMatch('styles: []');
+    debugger;
+    const result = preprocessFileContent(source, 'test.ts');
+    expect(result).not.toMatch('styleUrls');
+    expect(result).not.toMatch('styles');
     expect(result).toMatch(/template: require\(['"`]\.\/media-box-h0\.component\.html['"`]\)/);
   });
 
--- src/transformers/swc/swc-processor.ts
+++ src/transformers/swc/swc-processor.ts
@@ -1,14 +1,23 @@
-const TEMPLATE_URL_REGEX = /templateUrl\s*:\s*('|"|`)(\.\/){0,}(.*)('|"|`)/g;
-const STYLE_URLS_REGEX = /styleUrls\s*:\s*\[[^\]]*\]/g;
+const TEMPLATE_URL_REGEX = /\btemplateUrl\s*:\s*('|"|`)(\.\/){0,}(.*)('|"|`)/g;
+const STYLE_URLS_REGEX = /\s*\bstyleUrls\s*:\s*\[[^\]]*\][^\n]*/g;
+const STYLES_REGEX = /\s*\bstyles\s*:\s*\[[^\]]*\][^\n]*/g;
 const ESCAPE_TEMPLATE_REGEX = /(\${|\`)/g;
 
+const associatedTestFile = __filename.replace(/\.ts/, '.spec.ts');
+
 export function preprocessFileContent(fileContent: string, path: string): string {
-  if (path.endsWith('.html')) {
+  if (/\.html$/.test(path)) {
     fileContent = fileContent.replace(ESCAPE_TEMPLATE_REGEX, '\\$1');
+  } else if (/\.tsx?$/.test(path) && path !== associatedTestFile) {
+    fileContent = replaceTemplateAndStylesDecoratorProperties(fileContent);
   }
-  fileContent = fileContent
-    .replace(TEMPLATE_URL_REGEX, 'template: require($1./$3$4)')
-    .replace(STYLE_URLS_REGEX, 'styles: []');
 
   return fileContent;
+}
+
+function replaceTemplateAndStylesDecoratorProperties(fileContent: string): string {
+  return fileContent
+    .replace(TEMPLATE_URL_REGEX, 'template: require($1./$3$4)')
+    .replace(STYLE_URLS_REGEX, '')
+    .replace(STYLES_REGEX, '');
 }
