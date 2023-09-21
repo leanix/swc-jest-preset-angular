3e9971f30285241f26a3976f26d5e1ecd505e833
Rene Hamburger <rene.hamburger@leanix.net>
2023-09-21 10:05:02 +0200

    Revert "Make export objects configurable"
    This reverts commit 6efaa3caee1cba20a240f10610bff5768e1aa7c1.

--- src/transformers/swc/make-exports-configurable.ts
+++
@@ -1,6 +0,0 @@
-export function makeExportsConfigurable(code: string): string {
-  return code.replace(
-    /(?<=(^|\n)Object\.defineProperty\(exports, "\w+", \{\n    )enumerable: true,/g,
-    'enumerable: true, configurable: true,',
-  );
-}
